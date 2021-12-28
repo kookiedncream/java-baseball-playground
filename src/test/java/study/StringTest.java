b0e867aa84c868631078a0a6cfc3ddeecf770f92
kookiedncream <kim76744@naver.com>
2021-12-28 16:34:42 +0900

    test: String 클래스에 대한 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,37 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    /**
+     * String 클래스에 대한 학습 테스트
+     */
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String result = "(1,2)".substring(1,4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("chatAt()를 사용했을때 exception 종류 확인")
+    void charAt() {
+        assertThatThrownBy(() -> "abc".charAt(4))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
