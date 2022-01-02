4f7a2b10285e880e62e1c5e1e37f3a3d2adc3f0a
kookiedncream <kim76744@naver.com>
2022-01-02 20:23:17 +0900

    feat: 사용자의 입력 받는 기능 추가

---
+++ src/main/java/controller/InputController.java
@@ -0,0 +1,80 @@
+package controller;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class InputController {
+
+	private static Scanner scanner = new Scanner(System.in);
+	private static Set<Integer> number;
+
+	//입력기능
+	public InputController() {
+		this.number = input();
+	}
+
+	private Set<Integer> input() {
+		int value = scanner.nextInt();
+
+		validateNumber(value);
+
+		return intToSet(value);
+	}
+
+	private Set<Integer> intToSet(int value) {
+		Set<Integer> set = new HashSet<>();
+
+		set.add(value / 100);
+		value -= (value / 100) * 100;
+
+		set.add(value / 10);
+		value -= (value / 10) * 10;
+
+		set.add(value);
+
+		return set;
+	}
+
+	private void validateNumber(int value) {
+
+		isThreeDigitNumber(value);
+		haveNotZero(value);
+		notSameNumber(value);
+	}
+
+	private void notSameNumber(int value) {
+		String word = String.valueOf(value);
+		char firstWord = word.charAt(0);
+		char secondWord = word.charAt(1);
+		char thirdWord = word.charAt(2);
+
+		if ((firstWord == secondWord) ||
+				(firstWord == thirdWord) ||
+				(secondWord == thirdWord)) {
+			throw new IllegalStateException("서로 다른 숫자를 입력해 주세요");
+		}
+	}
+
+	private void haveNotZero(int value) {
+		String word = String.valueOf(value);
+
+		for (int i = 0; i < 3; i++) {
+			checkZero(word, i);
+		}
+	}
+
+	private void checkZero(String word, int i) {
+		if (word.charAt(i) == '0') {
+			throw new IllegalStateException("1 ~ 9사이의 숫자로 입력해 주세요");
+		}
+	}
+
+	private void isThreeDigitNumber(int value) {
+		if (value > 999 || value < 100) {
+			throw new IllegalStateException("3자리 수를 입력해 주세요");
+		}
+	}
+
+	//다시시작 입력 기능
+}
---
+++ src/main/java/view/Baseball.java
@@ -0,0 +1,18 @@
+package view;
+
+public class Baseball {
+
+	public Baseball() {
+		startGame();
+	}
+
+	private void startGame() {
+		System.out.println("게임 시작");
+	}
+
+	//입력 기능 프린트
+	public void inputMessage() {
+		System.out.print("숫자를 입력해 주세요 : ");
+	}
+	//다시 시작 기능 프린트
+}
