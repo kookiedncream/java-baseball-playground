73342075c4fea96ea1e63c15a4ae8220234cf172
kookiedncream <kim76744@naver.com>
2022-01-07 16:56:36 +0900

    feat: 서로 다른 3개의 숫자로 구성된 번호 생성 기능 추가

--- src/main/java/baseball/Balls.java
+++ src/main/java/baseball/Balls.java
@@ -2,6 +2,7 @@ package baseball;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Balls {
 
@@ -9,6 +10,31 @@ public class Balls {
 
 	public Balls(List<Integer> ball) {
 		this.balls = getBalls(ball);
+	}
+
+	public Balls() {
+		List<Integer> intBall = new ArrayList<>();
+		Random random = new Random();
+		getRandomBall(intBall, random);
+		this.balls = getBalls(intBall);
+	}
+
+	private void getRandomBall(List<Integer> intBall, Random random) {
+		while (intBall.size() < 3) {
+			int randomBall = random.nextInt(8) + 1;
+			makeRandomBall(intBall, randomBall);
+		}
+	}
+
+	private void makeRandomBall(List<Integer> intBall, int randomBall) {
+		if (doesItContain(intBall, randomBall)) {
+			return;
+		}
+		intBall.add(randomBall);
+	}
+
+	private boolean doesItContain(List<Integer> intBall, int randomBall) {
+		return intBall.contains(randomBall);
 	}
 
 	private List<Ball> getBalls(List<Integer> ball) {
@@ -31,5 +57,9 @@ public class Balls {
 				.filter(status -> status != BallStatus.NOTHING)
 				.findFirst()
 				.orElse(BallStatus.NOTHING);
+	}
+
+	public int getSize() {
+		return balls.size();
 	}
 }
